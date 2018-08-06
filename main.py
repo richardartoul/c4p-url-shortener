00dcb13fd2cae82d781c5699f591ee2a6b742189
Richard Artoul <richardartoul@gmail.com>
2018-08-06 11:49:26 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import logging
+
+from flask import Flask
+from flask import jsonify
+from flask import redirect
+from flask import request
+
+app = Flask(__name__)
+
+ERROR_ALREADY_EXISTS = "short_code_already_in_use"
+ERROR_SHORT_CODE_DOES_NOT_EXIST = "short_code_does_not_exist"
+
+shortened_urls = dict()
+
+logging.basicConfig()
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+@app.route("/", methods=["GET","POST"])
+def create_short_url():
+	if request.method == "POST":
+		return post_short_url()
+	if request.method == "GET":
+		return get_short_url()
+	raise Exception("unsupported method type")
+
+def post_short_url():
+	req = request.get_json()
+	url = req["url"]
+	short_code = req["short_code"]
+	if short_code in shortened_urls:
+		logger.debug(
+			"failed to create shortened URL, %s already exists",
+			short_code,
+		)
+		return error(ERROR_ALREADY_EXISTS), 400
+	
+	logger.debug(
+		"creating shortened URL, %s -> %s", url, short_code)
+	shortened_urls[short_code] = url
+	return jsonify({}), 200
+
+def get_short_url():
+	short_code = request.args["short_code"]
+	url = shortened_urls.get(short_code)
+	if not url:
+		return error(ERROR_SHORT_CODE_DOES_NOT_EXIST)
+	return redirect(url, code=302)
+
+def error(code):
+	return jsonify({"error_code": code})
+
+if __name__ == "__main__":
+	app.run()
+
